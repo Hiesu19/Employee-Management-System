d0d124019f20bd0ce2e7ffe9096afa79f76a01a0
Hiesu19 <thaihieu1919@gmail.com>
2025-07-07 14:17:33 +0700

    init & connect postgreSQL

---
+++ backend/src/config/db.config.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
+        dialect: process.env.DB_DIALECT || 'postgres',
+        logging: false,
+    }
+);
+
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connected to database successfully.');
+    } catch (error) {
+        console.log('Failed to connect to database:');
+    }
+};
+
+module.exports = connectDB;
