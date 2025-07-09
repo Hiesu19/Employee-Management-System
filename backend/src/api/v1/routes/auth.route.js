95c33d24be663ba8b6e73367fec732f4523e775f
Hiesu19 <thaihieu1919@gmail.com>
2025-07-09 15:02:15 +0700

    feat: them middleware

--- backend/src/api/v1/routes/auth.route.js
+++ backend/src/api/v1/routes/auth.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const AuthController = require('../controller/AuthController.controller');
+const { verifyToken, verifyTokenAndCheckRole } = require('../middleware/verify-token.middleware');
 
 router.post('/login', AuthController.login);
-router.post('/register', AuthController.register);
-router.post('/refresh-token', AuthController.refreshToken);
-router.post('/logout', AuthController.logout);
+router.post('/register', verifyTokenAndCheckRole(['root']), AuthController.register);
+router.post('/refresh-token', verifyToken, AuthController.refreshToken);
+router.post('/logout', verifyToken, AuthController.logout);
 
 module.exports = router;
