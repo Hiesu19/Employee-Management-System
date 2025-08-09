04f516f8b51fb95dbc4549e7372c0feea0955db1
Hiesu19 <thaihieu1919@gmail.com>
2025-08-09 08:28:10 +0700

    feat: thêm db-dev để chạy e2e

--- backend/src/config/sequelize.config.js
+++ backend/src/config/sequelize.config.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env',
+});
 
 module.exports = {
     development: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 5432,
-        dialect: process.env.DB_DIALECT || 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT || "postgres",
         logging: false,
     },
     test: {
